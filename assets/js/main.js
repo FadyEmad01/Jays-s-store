8be9d82888027e9d74b91abc01abca0f8c8b6143
fady emad <FadyEmad01>
2024-09-03 17:53:16 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let cart = document.querySelectorAll(".cart");
+let heart = document.querySelectorAll(".fa-heart");
+
+cart.forEach((cart, i) => {
+    cart.addEventListener("click", () => {
+        heart[i].classList.toggle("fa-regular");
+        heart[i].classList.toggle("fa-solid");
+    })
+});
+
+
