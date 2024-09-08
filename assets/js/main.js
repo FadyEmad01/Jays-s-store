6b2253a801bb815a610ca7e372abbf6accd94174
fady emad <FadyEmad01>
2024-09-08 17:36:43 +0300

    v3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,23 @@
-let cart = document.querySelectorAll(".cart");
-let heart = document.querySelectorAll(".fa-heart");
+// let love = document.querySelectorAll(".love");
+// let heart = document.querySelectorAll(".fa-heart");
 
-cart.forEach((cart, i) => {
-  cart.addEventListener("click", () => {
-    heart[i].classList.toggle("fa-regular");
-    heart[i].classList.toggle("fa-solid");
-  });
-});
+// love.forEach((love, i) => {
+//   love.addEventListener("click", () => {
+//     heart[i].classList.toggle("fa-regular");
+//     heart[i].classList.toggle("fa-solid");
+//   });
+// });
 
+
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
 
 
